60b880a1136bf20b77de1a6f816a9ffaf6d8bbe8
Shrived00 <sskakde_b21@pe.vjti.ac.in>
2025-02-07 11:08:09 +0530

    added: tiptap extensions

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -3,6 +3,12 @@ import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import TaskItem from '@tiptap/extension-task-item'
 import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image'
 
 
 export const Editor = () => {
@@ -18,8 +24,29 @@ export const Editor = () => {
             TaskItem.configure({
                 nested: true,
             }),
-            TaskList],
-        content: '<p>Hello World! 🌎️</p>',
+            TaskList,
+            Table,
+            TableRow,
+            TableHeader,
+            TableCell,
+            Image,
+            ImageResize
+
+        ],
+        content: ` <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>`,
     })
     return (
         <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
