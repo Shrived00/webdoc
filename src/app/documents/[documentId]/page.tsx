dd187b560b431361a2b03dcb86c5ce31db8477a4
Shrived00 <sskakde_b21@pe.vjti.ac.in>
2025-02-07 12:05:29 +0530

    toolbar

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Editor } from './editor';
+import { Toolbar } from './toolbar'
+
 interface DocumentIdPageProps {
     params: Promise<{ documentId: string }>;
 };
@@ -7,10 +9,12 @@ interface DocumentIdPageProps {
 
 const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
 
-    // const { documentId } = await params;
+    const { documentId } = await params;
+    console.log(documentId)
 
     return (
         <div className='min-h-screen bg-[#FAFBFD]'>
+            <Toolbar />
             <Editor />
         </div>
     )
