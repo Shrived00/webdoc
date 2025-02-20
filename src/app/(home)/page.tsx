f4aa0e22888aeb17564b6f0da2d4a476823afae6
Shrived00 <sskakde_b21@pe.vjti.acin>
2025-02-20 19:36:47 +0530

    add: new template files

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,21 +1,18 @@
-import Link from 'next/link'
-import { Navbar } from './navbar'
+import Link from "next/link";
+import { Navbar } from "./navbar";
+import TemplatesGallery from "./templates-gallery";
 
 const Home = () => {
   return (
     <div className="flex flex-col min-h-screen  ">
-
       <div className="fixed top-0 left-0 right-0 z-10 bg-white p-4">
         <Navbar />
       </div>
 
-      <div className="mt-16">
-
-        Click Here
-        <Link href='/documents/123' className='underline '> Go to document</Link>
+      <div className="mt-20">
+        <TemplatesGallery />
       </div>
-
     </div>
-  )
-}
-export default Home
+  );
+};
+export default Home;
