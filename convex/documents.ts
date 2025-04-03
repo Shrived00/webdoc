81673e1631c4f34432994d8d31bd97ac32b4b560
Shrived00 <shrivedkakde@gmail.com>
2025-04-03 23:30:05 +0530

    feat:users

--- convex/documents.ts
+++ convex/documents.ts
@@ -125,11 +125,20 @@ export const updateById = mutation({
     }
 
     const isOwner = document.ownerId === user.subject;
-    const isOrganizationMember = document.organizationId === organizationId;
+    const isOrganizationMember = !!(
+      document.organizationId && document.organizationId === organizationId
+    );
     if (!isOwner && !isOrganizationMember) {
       throw new ConvexError("User not authorized");
     }
 
     return await ctx.db.patch(args.id, { title: args.title });
+  },
+});
+
+export const getById = query({
+  args: { id: v.id("documents") },
+  handler: async (ctx, { id }) => {
+    return await ctx.db.get(id);
   },
 });
